ad7af7f362883fc6f233a3d824103fe01314e01c
Loris Manganelli <lorismanganelli05@gmail.com>
2024-05-21 21:57:03 +0200

    delete weird normalization

--- data/datamodule.py
+++ data/datamodule.py
@@ -13,18 +13,12 @@ class DataModule:
         real_images_val_path,
         train_transform1,
         train_transform2,
-        train_transform3,
-        train_transform4,
-        train_transform5,
         val_transform,
         batch_size,
         num_workers,
     ):
         self.dataset1 = ImageFolder(train_dataset_path, transform=train_transform1)
         self.dataset2 = ImageFolder(train_dataset_path, transform=train_transform2)
-        self.dataset3 = ImageFolder(train_dataset_path, transform=train_transform3)
-        self.dataset4 = ImageFolder(train_dataset_path, transform=train_transform4)
-        self.dataset5 = ImageFolder(train_dataset_path, transform=train_transform5)
 
         self.train_dataset1, self.val_dataset1 = torch.utils.data.random_split(
             self.dataset1,
@@ -44,37 +38,8 @@ class DataModule:
             generator=torch.Generator().manual_seed(3407),
         )
 
-        self.train_dataset3, self.val_dataset3 = torch.utils.data.random_split(
-            self.dataset3,
-            [
-                int(0.8 * len(self.dataset3)),
-                len(self.dataset3) - int(0.8 * len(self.dataset3)),
-            ],
-            generator=torch.Generator().manual_seed(3407),
-        )
-
-        self.train_dataset4, self.val_dataset4 = torch.utils.data.random_split(
-            self.dataset4,
-            [
-                int(0.8 * len(self.dataset4)),
-                len(self.dataset4) - int(0.8 * len(self.dataset4)),
-            ],
-            generator=torch.Generator().manual_seed(3407),
-        )
-
-        self.train_dataset5, self.val_dataset5 = torch.utils.data.random_split(
-            self.dataset5,
-            [
-                int(0.8 * len(self.dataset5)),
-                len(self.dataset5) - int(0.8 * len(self.dataset5)),
-            ],
-            generator=torch.Generator().manual_seed(3407),
-        )
-
-
-
-        self.combined_training_dataset = ConcatDataset([self.train_dataset1, self.train_dataset2, self.train_dataset3, self.train_dataset4, self.train_dataset5])
-        self.combined_val_dataset = ConcatDataset([self.val_dataset1, self.val_dataset2, self.val_dataset3, self.val_dataset4, self.val_dataset5])
+        self.combined_training_dataset = ConcatDataset([self.train_dataset1, self.train_dataset2])
+        self.combined_val_dataset = ConcatDataset([self.val_dataset1, self.val_dataset2])
 
 
         self.combined_val_dataset.transform = val_transform
