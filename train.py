1365bf5dcda19da042ee0af16a98ecf2410b8730
Bilulu <boudierluc@gmail.com>
2024-05-25 12:54:32 +0200

    New train saver

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ def train(cfg):
                 **val_metrics,
             }
         )
-        if(k%2 == 0 and k >=7 and k <= 25):
+        if(k%2 == 0 and k >=5 and k <= 16):
             torch.save(model.state_dict(), cfg.checkpoint_path + "_" + str(k) + ".pt")
         k=k+1
     torch.save(model.state_dict(), cfg.checkpoint_path + "_final.pt")
